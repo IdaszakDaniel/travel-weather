138e79ab6e15a18508586cd68412a4013fa0bf64
idaszakdaniel <idaszak1@gmail.com>
2018-05-06 11:27:43 +0200

    ActivityIndicator added to map

--- mapComponent.js
+++ mapComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { MapView } from 'expo';
+import { ActivityIndicator, View } from 'react-native';
 
 export default class Map extends React.Component {
 
     state = {
+        mapLoaded: false,
         region: {
             longitude: 16.93,
             latitude: 52.40,
@@ -12,12 +14,25 @@ export default class Map extends React.Component {
         }
     }
 
+    componentDidMount(){
+        this.setState({mapLoaded: true})
+    }
+
     render() {
+
+        if(!this.state.mapLoaded){
+            return(
+                <View style={{flex: 1, justifyContent: 'center'}}>
+                    <ActivityIndicator size="large"/>
+                </View>
+            )
+        }
+
         return (
             <MapView 
                 style={{flex: 1}}
                 region={this.state.region}
-            />
+            />    
         );
     }
 }
